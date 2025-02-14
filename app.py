ef92df5a77373cb83269f45b80c1c979f0f01690
skamp931 <mythappy622@gmail.com>
2025-02-15 00:13:12 +0900

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_dividends_from_minkabu(stock_code):
     # 配当金データを取得
     dividend_elements = soup.find("span", class_="dividend-state__amount__integer")
     st.write(dividend_elements.contents)
-    dividend = dividend_elements.contents.strip().replace('.', '')  # 小数点を削除して整数部分のみ取得
+    dividend = str(dividend_elements.contents).replace('.', '')+"円"
     
     return dividend
 
